7c0a6d5aada6c505ad84bdb87f8cc090ed0651cb
andrew <andrewcvolle@gmail.com>
2026-02-03 18:02:11 +0100

    inpmlement functions to v1

--- courses/coding-in-C/Lab_2/advanced-chessboard.c
+++ courses/coding-in-C/Lab_2/advanced-chessboard.c
@@ -3,9 +3,17 @@
 char *letters = {"ABCDEFGH"};
 
 void printLetters();
-void printRows(int k);
+void printRow(int k);
+void printDividers();
 
 int main(){
+    printLetters();
+
+    for(int i = 8; i > 0; i--){
+        printDividers();
+        printRow(i);
+    }
+
     printLetters();
 
 }
@@ -18,14 +26,34 @@ void printLetters(){
         printf("%c", letters[i]);
     }
     printf("\n");
-
-
 }
 
-void printRows(int k){
-    
+void printRow(int k){
+    printf("%d", k);
+    printf(" ");
+
+    for(int i = 0; i<4; i++){
+        if(!(k%2)){
+            printf("|###");
+            printf("|   ");
+
+        } else{
+            printf("|   ");
+            printf("|###");
+        }
+
+    }
+    printf("| ");
+    printf("%d", k);
+    printf("\n");
 }
 
 void printDividers(){
+    printf("  ");
+    for(int i = 0; i <8; i++){
+        printf("+---");
+    }
+    printf("+");
+    printf("\n");
 
 }
