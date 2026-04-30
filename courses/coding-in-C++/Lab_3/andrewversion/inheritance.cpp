dc3802555d7f9684b0d721062c4fc2b3b0d7de11
andrew <andrewcvolle@gmail.com>
2026-04-30 09:07:02 +0200

    did most of lab 2

---
+++ courses/coding-in-C++/Lab_3/andrewversion/inheritance.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+
+class User{
+    protected:
+        std::string name;
+        int id;
+    
+    public:
+        User(std::string name, int id) : 
+        name(name), id(id){}
+
+        void printInfo(){
+            std::cout << "Name: " << name << std::endl;
+            std:: cout << "ID: " << id << std::endl;
+        }
+
+};
+
+class Student : public User{
+    protected:
+        double grade;
+    
+    public:
+        Student(std::string name, int id, double grade) : User(name, id), grade(grade){}
+        void printRole(){
+            printInfo();
+            std::cout << "Grade: " << grade <<std::endl;
+        }
+};
+
+class Instructor : public User{
+    private: 
+        double salary;
+    
+    public:
+        Instructor(std::string name, int id, double salary) : User(name, id), salary(salary){}
+        void printRole(){
+            printInfo();
+            std::cout << "Salary: " << salary << std::endl;
+        }
+};
+
+int main(){
+    Student a = Student("Andrew Volle", 1, 1.0);
+    Instructor b = Instructor("Dr. Braunagel", 2, 1000000000);
+
+    a.printInfo();
+    a.printRole();
+
+    b.printInfo();
+    b.printRole();
+
+    
+
+}
